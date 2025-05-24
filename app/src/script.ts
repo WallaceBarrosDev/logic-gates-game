f80250c8fe13f5633e4d8de6660142b945d00751
WallaceBarrosDev <w.wallacebarro.cruz@gmail.com>
2025-05-24 17:58:24 -0300

    elemento arastavel

--- app/src/script.ts
+++ app/src/script.ts
@@ -7,37 +7,59 @@ const deleteCard = document.querySelector("#delete") as HTMLDivElement;
 
 var draggedCard: HTMLDivElement;
 
-const dragStartClone = ({ target }: DragEvent) => {
-  const card = target as HTMLDivElement
-  draggedCard = card.cloneNode(true) as HTMLDivElement;
+function createGhost() {
+  const ghost = document.createElement("div");
+  ghost.style.width = "0px";
+  ghost.style.height = "0px";
+  ghost.style.opacity = "0";
+  document.body.appendChild(ghost);
+  return ghost;
 }
 
-const dragstart = ({ target }: DragEvent) => {
-  draggedCard = target as HTMLDivElement
+function removeGhost(event: DragEvent) {
+  const ghost = createGhost()
+  event.dataTransfer?.setDragImage(ghost, 0, 0);
+  setTimeout(() => ghost.remove(), 0);
+}
+
+const dragStartClone = (event: DragEvent) => {
+  const card = event.target as HTMLDivElement
+  draggedCard = card.cloneNode(true) as HTMLDivElement;
+    if (app.classList.contains('app')) {
+    draggedCard.addEventListener('dragstart', dragstart)
+    draggedCard.addEventListener('drag', drag)
+    app.appendChild(draggedCard)
+  }
+  removeGhost(event)
+}
+
+const dragstart = (event: DragEvent) => {
+  draggedCard = event.target as HTMLDivElement
+  removeGhost(event)
 }
 
 const drageOver = (event: DragEvent) => {
   event.preventDefault()
 }
 
-const drop = ({ target }: DragEvent) => {
-  const dropApp = target as HTMLDivElement
-  if (dropApp.classList.contains('app')) {
-    draggedCard.addEventListener('dragstart', dragstart)
-    dropApp.appendChild(draggedCard)
-  }
-}
-
 const dropDelete = () => {
   draggedCard.remove()
 }
 
+const drag = (event: MouseEvent) => {
+  const position_x = event.clientX;
+  const position_y = event.clientY;
+  
+  draggedCard.style.top = (position_y - 75) + "px"
+  draggedCard.style.left = (position_x - 25) + "px"
+}
+
 cards.forEach(card => {
   card.addEventListener('dragstart', dragStartClone)
+  card.addEventListener('drag', drag)
 });
 
 app.addEventListener('dragover', drageOver)
-app.addEventListener('drop', drop)
 
 deleteCard.addEventListener('dragover', drageOver)
 deleteCard.addEventListener('drop', dropDelete)
--- app/src/services/and.service.ts
+++
@@ -1,3 +0,0 @@
-export function AND(a: boolean, b: boolean): boolean {
-  return a && b;
-}
